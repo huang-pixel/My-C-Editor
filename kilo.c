9e4b4a103358af5356c2431dd3dbd8bb195a35a1
huang-pixel <he.huang.h@outlook.com>
2025-06-22 17:37:57 +0200

    Close canonical display key ascii

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -23,7 +25,7 @@ void enableRawMode() {
 	// ~(ECHO): NOT ECHO filp the bits
 	// &=: bitwise AND then store the result back
 	struct termios raw = orig_termios;
-	raw.c_lflag &= ~(ECHO);
+	raw.c_lflag &= ~(ECHO | ICANON);
 
         // apply modified settings
 	// tcsetattr: terminal control set attr
@@ -35,6 +37,13 @@ int main() {
 	enableRawMode();
 	
 	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+		if (iscntrl(c)) {
+			printf("%d\n", c);
+		} else {
+			printf("%d ('%c')\n", c, c);
+		}
+	}
+
 	return 0;
 }
